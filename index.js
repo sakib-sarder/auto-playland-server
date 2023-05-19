745287ee61932fdd640dfaf7331934798ef0e322
sakib <s.sarder232002@gmail.com>
2023-05-20 05:33:46 +0600

    delete operation implemented

--- index.js
+++ index.js
@@ -70,6 +70,13 @@ async function run() {
       res.send(result);
     });
 
+    app.delete("/my-toys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toyCollection.deleteOne(query);
+      res.send(result);
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!"
